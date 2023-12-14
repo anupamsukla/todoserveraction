b31fe18982343716dcc2ca789c52173068564abc
anupam anand <anupam.a@m2pfintech.com>
2023-12-14 12:22:11 +0530

    hi

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -23,7 +23,6 @@ const TodoItem = ({ todo }: TodoItemProps) => {
         className='peer h-4 w-4 cursor-pointer rounded border-gray-300 text-slate-600 focus:ring-slate-600'
       />
       <label
-        htmlFor={todo.id}
         className='cursor-pointer peer-checked:text-slate-500 peer-checked:line-through'
       >
         {todo.title}
